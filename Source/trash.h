5573a4d075d403a5b7620e3a64f02d48c6f5b871
txesmi <txesmi@gmail.com>
2013-06-19 15:23:17 +0200

    Reupdated documentation :D

--- Source/trash.h
+++ Source/trash.h
@@ -1,17 +1,37 @@
 
-/* 
-   The Ultimate Script Library
-   -----------------------------------------------------------------------------
+/* The Ultimate Script Library 
+-----------------------------------------------------------------------------------
    Module: Trash
- 
+
    Read the documentation for further information: TUST\Documentation\index.html
-   -----------------------------------------------------------------------------
- */
+----------------------------------------------------------------------------------- 
+*/
 
 #ifndef _TRASH_H_
 #define _TRASH_H_
 
+/**
+ * \defgroup TrashGroup Trash
+ * \ingroup GenericScriptLibrary
+ * \brief Linked trash or stack implementation.
+ * \{
+ * \file trash.h
+ * \brief Linked trash or stack implementation header.
+ * \file trash.c
+ * \brief Linked trash or stack implementation code.
+ */
+
+/**
+ * \typedef void TrashData
+ * \brief Used instead of void to give a clear difference between a normal pointer and a pointer used in trash.
+ */
 typedef void TrashData;
+
+/**
+ * \typedef void TrashRemover
+ * \brief Used instead of void to give a clear difference between a normal function and a remover function used in trash.
+ */
+typedef void TrashRemover;
 
 typedef struct TrashItem
 {
@@ -19,8 +39,10 @@ typedef struct TrashItem
 	TrashData *data;
 } TrashItem;
 
-typedef void TrashRemover;
-
+/**
+ * \class TrashCollector
+ * \brief Generic trash struct
+ */
 typedef struct TrashCollector
 {
 	int count;
@@ -29,86 +51,64 @@ typedef struct TrashCollector
 	TrashRemover delete_function ( TrashData *data );
 } TrashCollector;
 
+/**
+ * \}
+ *
+ * \var TrashCollector::count
+ * \brief Amount of items cointaned by the trash.
+ * \warning Read only variable
+ *
+ * \addtogroup TrashGroup
+ * \{
+ */
+
+/**
+ * \brief Open the trash system
+ */
 void trash_open ();
 
+/**
+ * \brief Close the trash system
+ */
 void trash_close ();
 
-TrashCollector *trash_create ( int items_max, TrashRemover *remove_function );
-
-void trash_add ( TrashCollector *trash, TrashData *data );
-
-TrashData *trash_recover ( TrashCollector *trash );
-
-void trash_clear ( TrashCollector *trash );
-
-void trash_remove ( TrashCollector *trash );
-
-
-// DOXYGEN 
 /**
- * \defgroup TrashGroup Trash
- * \ingroup GenericScriptLibrary
- * \brief Linked trash or stack implementation.
- *
- * \file trash.h
- * \ingroup TrashGroup
- * \brief Linked trash or stack implementation.
- *
- * \file trash.c
- * \ingroup TrashGroup
- * \brief Linked trash or stack implementation.
- *
- * \typedef void TrashData
- * \brief Used instead of void to give a clear difference between a normal pointer and a pointer used in trash.
- * \ingroup TrashGroup
- *
- * \typedef void TrashRemover
- * \brief Used instead of void to give a clear difference between a normal function and a remover function used in trash.
- * \ingroup TrashGroup
- *
- * \class TrashCollector
- * \ingroup TrashGroup
- * \brief Generic trash struct
- * \var TrashCollector::count
- * \brief Amount of item cointaned by the trash. (read only)
- *
- * \fn trash_open ()
- * \brief Open the trash system
- * \ingroup TrashGroup
- *
- * \fn trash_close ()
- * \brief Close the trash system
- * \ingroup TrashGroup
- *
- * \fn trash_create ( int items_max, TrashRemover *remove_function )
  * \brief Creates a new trash.
- * \ingroup TrashGroup
  * \param   items_max         The maximum amount of items.
  * \param   remove_function   The trash to delete.
  * \return                    A new trash.
- *
- * \fn trash_add ( TrashCollector *trash, TrashData *data )
+ */
+TrashCollector *trash_create ( int items_max, TrashRemover *remove_function );
+
+/**
  * \brief Adds a new item to the trash.
- * \ingroup TrashGroup
  * \param   trash             The trash where the item should be added.
  * \param   item              The item which should be added to the trash.
- *
- * \fn trash_recover ( TrashCollector *trash )
+ */
+void trash_add ( TrashCollector *trash, TrashData *data );
+
+/**
  * \brief Get a trash item.
- * \ingroup TrashGroup
  * \param   trash             The trash to get the item from.
  * \return                    First item into the trash if exists, otherwise NULL
- *
- * \fn trash_clear ( TrashCollector *trash )
+ */
+TrashData *trash_recover ( TrashCollector *trash );
+
+/**
  * \brief Removes all items from a trash.
- * \ingroup TrashGroup
  * \param   trash             The trash which should be cleared.
- *
- * \fn trash_remove ( TrashCollector *trash )
+ */
+void trash_clear ( TrashCollector *trash );
+
+/**
  * \brief   Deletes a trash.
- * \ingroup TrashGroup
  * \param   trash             The trash to delete.
  */
- 
+void trash_remove ( TrashCollector *trash );
+
 #include "trash.c"
 #endif
+
+/**
+ * \}
+ */
