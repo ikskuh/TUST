5e485d6609214125cae3aa12853c2f58a8f04ac9
padmalcom <j.freiknecht@googlemail.com>
2013-07-02 00:09:20 +0200

    Changed the procedural street generation.

--- Tests/test_procedural.c
+++ Tests/test_procedural.c
@@ -88,6 +88,12 @@ void create_small_streets()
 #define VO_MIN_Y -1000
 #define VO_MAX_Y 1000
 
+typedef struct {
+	VECTOR *pos;
+	int incomingStreets;
+	List *incomingAngles;
+} Intersection;
+
 void create_random_streets()
 {
 	int i, j;
@@ -96,145 +102,132 @@ void create_random_streets()
 	vec_set(camera.x, vector(15, 1014, 698));
 	vec_set(camera.pan, vector(270, -35, 0));
 	
-	ENTITY* entTerrain = ent_create("..\\Ressources\\Terrains\\helymap1.hmp", vector(0,0,0), NULL);
+	//ENTITY* entTerrain = ent_create("..\\Ressources\\Terrains\\helymap1.hmp", vector(0,0,0), NULL);
 	BMAP* bmapStreetTexture = bmap_create("..\\Ressources\\Graphics\\street.tga");
 
 	// Create a road network using voronoi diagrams
 	random_seed(0);
-	vo_init();	
-	for(i=0; i<40; i++) {
+	vo_init();
+	for(i=0; i<6; i++) {
 		vo_add_point(VO_MIN_X + integer(random(2 * VO_MAX_X)), VO_MIN_Y + integer(random(2 * VO_MAX_Y)));
 	}
 	vo_execute(VO_MIN_X, VO_MAX_X, VO_MIN_Y, VO_MAX_Y, 3);
 	int nResults = vo_get_result_count();
+	printf("Lines: %i", nResults);
 	
 	// Create a list of streets
 	List *streets = list_create();
 	
 	// Positions that have already been identified as intersections
 	// can be ignored
-	List *intersections = list_create();
+	List *intersections = list_create(); // Type: Intersection
 	
 	// A temporary street that is filled so long with points until
 	// an intersection is reached
-	Street *currentStreet = street_create(25, 5);
+	Street *currentStreet = street_create(25, 5); // Type: Street
 	
 	
 	float x1, y1, x2, y2, x3, y3, x4, y4;
 	int nIncomingStreets = 0;
 	
+	BOOL bFoundOne = false;
+	BOOL bFoundTwo = false;
+	
+	TEXT* txtIntersections = txt_create(300, 10);
+	set(txtIntersections, SHOW);
+		
 	for(i=0; i<nResults; i++) {
 		nIncomingStreets = 0;
 		vo_get_result_at(i, &x1, &y1, &x2, &y2);
 		
-		// Is intersection?
-		for(j=0; j<nResults; j++) {
+		bFoundOne = false;
+		bFoundTwo = false;
+		for (j=0; j<list_get_count(intersections); j++) {
+		
+			Intersection *tempInter = list_item_at(intersections, j);
 			
-			VECTOR* vecNewIntersection = NULL;
+			// Found an intersection at a known point (1st end)
 			
-			// Check all lines except i
-			if (j != i) {
-				vo_get_result_at(j, &x3, &y3, &x4, &y4);
-				
-				// Check for same positions
-				if ((x1 == x3 && y1 == y3) || (x1 == x4 && y1 == y4)) {
-					
-					// If it is a newly found intersection
-					if (nIncomingStreets == 0)	{
-						
-						// Intersection found --> end current street segment
-						list_add(streets, currentStreet);
-						
-						// ... and create a new one!
-						currentStreet = street_create(25, 5);
-						
-						// Does this intersection already exist?
-						int nExists = 0;
-						ListIterator *it = list_begin_iterate(intersections);
-						VECTOR* vecTemp;
-						for(vecTemp = list_iterate(it); it->hasNext; vecTemp = list_iterate(it))
-						{
-							if (vecTemp.x == x1 && vecTemp.y == y1) {
-								nExists = 1;
-								break;
-							}
-						} 
-						list_end_iterate(it);
-						
-						// No? Then save intersection position
-						if (nExists == 0) {
-							vecNewIntersection = sys_malloc(sizeof(VECTOR));
-							list_add(intersections, vecNewIntersection);
-						}
-					}
-					
-					nIncomingStreets++;
-					
-					// Z position names incoming street segments
-					vecNewIntersection.z = nIncomingStreets;
-				}
-				
-				
-				// Same for the second point
-				if ((x2 == x3 && y2 == y3) || (x2 == x4 && y2 == y4)) {
-					
-					// If it is a newly found intersection
-					if (nIncomingStreets == 0)	{
-						
-						// Intersection found --> end current street segment
-						list_add(streets, currentStreet);
-						
-						// ... and create a new one!
-						currentStreet = street_create(25, 5);
-						
-						// Does this intersection already exist?
-						int nExists = 0;
-						ListIterator *it = list_begin_iterate(intersections);
-						VECTOR* vecTemp;
-						for(vecTemp = list_iterate(it); it->hasNext; vecTemp = list_iterate(it))
-						{
-							if (vecTemp.x == x2 && vecTemp.y == y2) {
-								nExists = 1;
-								break;
-							}
-						} 
-						list_end_iterate(it);
-						
-						// No? Then save intersection position
-						if (nExists == 0) {
-							vecNewIntersection = sys_malloc(sizeof(VECTOR));
-							list_add(intersections, vecNewIntersection);
-						}
-					}
-					
-					nIncomingStreets++;
-					
-					// Z position names incoming street segments
-					vecNewIntersection.z = nIncomingStreets;					
-				}
+			if ((floatv(tempInter->pos->x) == x1) && (floatv(tempInter->pos->y) == y1) && (bFoundOne == false)) {
+				bFoundOne = true;
+				tempInter->incomingStreets +=1;
+				VECTOR* vecNewAngle = sys_malloc(sizeof(VECTOR));
+				vec_set(vecNewAngle, vector(0,0,0));
+				list_add(tempInter->incomingAngles, vecNewAngle);
+				list_add(intersections, tempInter);
+			}
+			
+			// Found an intersection at a known point (2nd end)
+			if ((floatv(tempInter->pos->x) == x2) && (floatv(tempInter->pos->y) == y2) && (bFoundTwo == false)) {
+				bFoundTwo = true;
+				tempInter->incomingStreets +=1;
+				VECTOR* vecNewAngle = sys_malloc(sizeof(VECTOR));
+				vec_set(vecNewAngle, vector(0,0,0));
+				list_add(tempInter->incomingAngles, vecNewAngle);
+				list_add(intersections, tempInter);		
 			}
 		}
 		
-		// If there are not several streets at the current position...
-		if (nIncomingStreets == 0) {
-			
-			// Add a street
-			street_addpos(currentStreet, vector(x1,y2,0));
+		if (bFoundOne == false) {
+			Intersection *newInter = sys_malloc(sizeof(Intersection));
+			newInter->incomingAngles = list_create();
+			newInter->pos = sys_malloc(sizeof(VECTOR));
+			newInter->pos->x = x1;
+			newInter->pos->y = y1;
+			newInter->incomingStreets = 1;
+			list_add(intersections, newInter);
+			str_cpy((txtIntersections.pstring)[list_get_count(intersections)], str_for_num(NULL, newInter->pos->x));
+			str_cat((txtIntersections.pstring)[list_get_count(intersections)], ",");
+			str_cat((txtIntersections.pstring)[list_get_count(intersections)], str_for_num(NULL, newInter->pos->y));
 		}
 		
+		if (bFoundTwo == false) {
+			Intersection *newInter = sys_malloc(sizeof(Intersection));
+			newInter->incomingAngles = list_create();
+			newInter->pos = sys_malloc(sizeof(VECTOR));
+			newInter->pos->x = x2;
+			newInter->pos->y = y2;
+			newInter->incomingStreets = 1;
+			list_add(intersections, newInter);
+			str_cpy((txtIntersections.pstring)[list_get_count(intersections)], str_for_num(NULL, newInter->pos->x));
+			str_cat((txtIntersections.pstring)[list_get_count(intersections)], ",");
+			str_cat((txtIntersections.pstring)[list_get_count(intersections)], str_for_num(NULL, newInter->pos->y));
+		}
 	}
+	
+	int count = list_get_count(intersections);
+	printf("Count %i", count);
 		
+		
+		while(!key_esc) {
+		
+		// Draw diagram
+		for(i=0; i<nResults; i++) {
+			vo_get_result_at(i, &x1, &y1, &x2, &y2);
+			
+			draw_line3d(vector(x1, y1, 0), NULL, 100);
+			draw_line3d(vector(x2, y2, 0), COLOR_RED, 100);
+			
+		}
+		
+		/*for(i=0; i<nPoints; i++) {
+			vo_get_point(i, &x1, &y1);
+			draw_point(x1, y1);
+		}*/
+		wait(1);
+	}
+	
 
 	vo_free();		
 
 	// "Draw" all streets
 	
-	ListIterator *it = list_begin_iterate(streets);
+	/*ListIterator *it = list_begin_iterate(streets);
 	Street* tempStreet;
 	for(tempStreet = list_iterate(it); it->hasNext; tempStreet = list_iterate(it))
 	{
 		street_build(tempStreet, bmapStreetTexture);
-	} 
+	} */
 }
 
 void main() {
